59eb91bb596dbda27b57a89c125ac81c0ed1ecc8
AngelaLuu <111194371+AngelaLuu@users.noreply.github.com>
2025-05-22 16:41:38 -0500

    Update Rectangulo.java

--- src/solid/srp/solid/ocp/Rectangulo.java
+++ src/solid/srp/solid/ocp/Rectangulo.java
@@ -1,5 +1,16 @@
 package solid.ocp;
 
-public class Rectangulo {
+public class Rectangulo extends Forma {
+    private double ancho;
+    private double alto;
     
+    public Rectangulo(double ancho, double alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+    
+    @Override
+    public double calcularArea() {
+        return ancho * alto;
+    }
 }
