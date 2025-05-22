09f194310938b9e93e0dd22f924e994648080cc8
AngelaLuu <111194371+AngelaLuu@users.noreply.github.com>
2025-05-22 16:35:20 -0500

    Update ServicioEmail.java

---
+++ src/solid/srp/ServicioEmail.java
@@ -0,0 +1,7 @@
+package solid.srp;
+
+public class ServicioEmail {
+    public void enviar(String email, String mensaje) {
+        System.out.println("Enviando email a: " + email + " - " + mensaje);
+    }
+}
