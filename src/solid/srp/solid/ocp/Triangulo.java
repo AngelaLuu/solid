bb341dff86e24c804c63aed7e3cf2a6df62e20b7
AngelaLuu <111194371+AngelaLuu@users.noreply.github.com>
2025-05-22 16:43:23 -0500

    Update Triangulo.java

--- src/solid/srp/solid/ocp/Triangulo.java
+++ src/solid/srp/solid/ocp/Triangulo.java
@@ -1,5 +1,16 @@
 package solid.ocp;
 
-public class Triangulo {
+public class Triangulo extends Forma {
+    private double base;
+    private double altura;
     
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    
+    @Override
+    public double calcularArea() {
+        return (base * altura) / 2;
+    }
 }
