f8111a3fb756700e95c060904bb3f5b9afa24991
AngelaLuu <111194371+AngelaLuu@users.noreply.github.com>
2025-05-22 16:41:48 -0500

    Delete src/solid/srp/ServicioEmail.java

--- src/solid/srp/ServicioEmail.java
+++
@@ -1,7 +0,0 @@
-package solid.srp;
-
-public class ServicioEmail {
-    public void enviar(String email, String mensaje) {
-        System.out.println("Enviando email a: " + email + " - " + mensaje);
-    }
-}
